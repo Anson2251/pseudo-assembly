cc86a174638fee7e8cbf53c2db331c35b6940744
Heyan Zhu <zhu_heyan@163.com>
2024-09-19 10:40:15 +0800

    add io overflow handling test

--- test/machine/io.test.ts
+++ test/machine/io.test.ts
@@ -26,4 +26,30 @@ suite('Interpreter-Core-Machine - IO Instructions', () => {
         await vm.execute(instructions);
         expect(output).toBe(15);
     });
+
+    it('should be able to handle overflow', async () => {
+        const vm = new machine(8);
+
+        let output = -1;
+
+        vm.addDevice('output', async (value: number) => {
+            output = value;
+        });
+
+        vm.addDevice('input', async () => {
+            return 256;
+        });
+
+        vm.verbose = false;
+
+        const instructions = [
+            { opcode: 0b00010000, operand: 0b00000000 }, // IN
+            { opcode: 0b00010001, operand: 0b00000000 }, // OUT
+            { opcode: 0b00110011, operand: 0b00000000 }  // END
+        ];
+
+        await vm.execute(instructions);
+        vm.logMemoryAndRegisters()
+        expect(output).toBe(0b00000000);
+    });
 });
